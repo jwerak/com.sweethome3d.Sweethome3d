640101ecff16cf71b6ea55f8b108adbbfda6b84b
Bastien Nocera <hadess@hadess.net>
2026-08-29 00:26:57 +0200

    Remove bundle creation in build script
    It took too much time, and created a bundle that required GPG signing
    without signing it, making it uninstallable.

--- local-build.sh
+++ local-build.sh
@@ -7,5 +7,3 @@ rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
 flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.sweethome3d.Sweethome3d.json --repo=_repo
-flatpak build-bundle _repo com.sweethome3d.Sweethome3d.flatpak com.sweethome3d.Sweethome3d $BRANCH
-
